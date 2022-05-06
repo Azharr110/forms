31d257d22101d73abc06bb809e72f85f35095c70
Azhar <abdazhar786@gmail.com>
2022-05-06 16:52:19 +0530

    icon alert filled added

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import DataTable from "react-data-table-component";
 import "./App.css";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { EditOutlined, DeleteOutlined, AlertFilled } from "@ant-design/icons";
 
 const columns = (removeItem, editItem) => [
   {
